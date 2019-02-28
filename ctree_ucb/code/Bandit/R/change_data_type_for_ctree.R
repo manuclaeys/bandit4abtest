0e51dfa118c64d34573141b0b26138e44423c728
maje <marin.brunel@epitech.eu>
2019-02-28 17:45:47 +0100

    [~] Modified the file for the package R

--- ctree_ucb/code/Bandit/R/change_data_type_for_ctree.R
+++ ctree_ucb/code/Bandit/R/change_data_type_for_ctree.R
@@ -1,15 +1,24 @@
 
-change_data_type_for_ctree <- function(dt,is_reward_are_boolean,visitorReward){
-
-  #change type of reward data if it's consider as boolean values
-  if(is_reward_are_boolean==TRUE){
-    for(i in 1:ncol(visitorReward)) visitorReward[,i] <- as.factor(visitorReward[,i] )
+change_data_type_for_ctree <- function(dt, is_reward_boolean, visitorReward) {
+  # Change the type of reward data if it is consider as boolean values
+  #
+  # Args:
+  #   dt: the dataframes, or reward value
+  #   is_reward_boolean: tell if the dataframes, or reward, is of boolean type
+  #   visitorReward: dataframe of reward
+  #
+  # Returns:
+  #   
+  #
+  # Error handling
+  if (is_reward_boolean) {
+    for (i in 1:ncol(visitorReward))
+      visitorReward[, i] <- as.factor(visitorReward[, i])
   }
-
-  for(i in 1:ncol(dt)){
-    if(((typeof(dt[,i])=="character") | (typeof(dt[,i])=="logical"))==TRUE) dt[,i] <- as.factor(dt[,i])
+  for (i in 1:ncol(dt)) {
+    if ((typeof(dt[, i]) == "character") | (typeof(dt[, i]) == "logical"))
+      dt[,i] <- as.factor(dt[,i])
   }
-
   return(list(dt = dt,visitorReward=visitorReward ))
 }
 
