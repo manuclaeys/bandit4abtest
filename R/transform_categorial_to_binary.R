10523065f79183ffc02c51ac13f2711d7c378a49
manuclaeys <claeys.esiea@gmail.com>
2022-10-25 09:33:20 +0200

    transform categorial to binary

--- R/transform_categorial_to_binary.R
+++ R/transform_categorial_to_binary.R
@@ -21,9 +21,9 @@ transform_categorial_to_binary <- function(listCategorial=0 , listInteger=0,dt){
 
    dt<- as.data.frame(dt)
 
-  if( listCategorial==0 ) return(dt)
+  if( listCategorial[1]==0 ) return(dt)
 
-  if( listInteger==0 ){
+  if( listInteger[1]==0 ){
     if(length(listCategorial)==1) return(vect_to_matrix_transform(dt[,listCategorial]))  ###A changer
     return(cbind(model.matrix(~ . + 0, data=dt[,listCategorial] ,
                               contrasts.arg = lapply(dt[,listCategorial] , contrasts, contrasts=FALSE))))
